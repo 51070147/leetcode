d7655e1388fe3f2b1760d75272a50ba09a0000d4
wuze <51070147@qq.com>
2018-07-07 21:00:48 +0800

    找到字符串中所有字母异位词 初始化

---
+++ Find All Anagrams in a String/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2018-07-07 21:00:10
+# @Author  : Your Name (you@example.org)
+# @Link    : http://example.org
+# @Version : $Id$
+
+import os
+
+
+
+if __name__=='__main__':
+	print(111)
