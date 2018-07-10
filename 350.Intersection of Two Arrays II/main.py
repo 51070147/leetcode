77224ae5de038293940152cd424dc27b1281e0a4
wuze <51070147@qq.com>
2018-07-10 20:24:47 +0800

    438考题完成

--- 350.Intersection of Two Arrays II/main.py
+++ 350.Intersection of Two Arrays II/main.py
@@ -6,3 +6,24 @@
 # @Version : $Id$
 
 import os
+
+
+ # 思路- 然后遍历nums2  在nums1中  ，就输出到list中
+
+class Solution:
+	def intersect(self, nums1, nums2):
+		"""
+		:type nums1: List[int]
+		:type nums2: List[int]
+		:rtype: List[int]
+		"""
+		resultList=[]
+		for i in nums2:
+			if i in nums1:
+				nums1.remove(i)
+				resultList.append(i)			
+		return resultList
+
+
+if __name__=='__main__':
+	print(Solution.intersect('',[1],[1,1]))
